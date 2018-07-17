7299d93353d0f7dce3e8d10adb93b16e7b7d1c4f
Andrew Aganad <andrew.aganad@gmail.com>
2018-07-16 18:53:58 -0700

    Variables exercises

---
+++ variables/exercise_5.rb
@@ -0,0 +1,3 @@
+#In the first case, x prints 3
+
+#In the second case, there's an error because x cannot be accessed from the outside
---
+++ variables/name.rb
@@ -0,0 +1,15 @@
+# exercise 1
+name = gets.chomp
+puts "Good morning, #{name}!"
+
+# exercise 3 
+10.times do 
+	puts name 
+end
+
+# exercise 4
+puts "What is your first name?"
+first_name = gets.chomp
+puts "What is your last name?"
+last_name = gets.chomp
+puts "#{first_name} #{last_name}"
